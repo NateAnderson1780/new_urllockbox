94d4694aabb3b5688c9170b1266b1aace84a2e8e
Nate Anderson <curtislowe10@gmail.com>
2017-01-23 17:44:36 -0700

    add flash messages to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,10 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:success] = 'Welcome to URLLOCKBOX !!!!'
       redirect_to :root
     else
+      flash[:error] = 'Your email and/or password is incorrect, please try again'
       redirect_to :login
     end
   end
