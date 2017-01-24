b7f8d60b37bd39169d1719f913f7c16116a3240f
Nate Anderson <curtislowe10@gmail.com>
2017-01-23 17:45:19 -0700

    add flash messages to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,15 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = 'Welcome to URLLOCKBOX !!!!'
       redirect_to :root
     else
+      flash[:error] = user.errors.full_messages.join(", ")
       redirect_to :signup
     end
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
