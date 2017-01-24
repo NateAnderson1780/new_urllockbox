7bfe8d4c650c9cdeba00832989383700d1dc9b6f
Nate Anderson <curtislowe10@gmail.com>
2017-01-23 17:43:53 -0700

    add edit and update methods to links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,26 @@
 class LinksController < ApplicationController
   def index
     @hot_links = Link.hot
+    @links = current_user.links.all
+  end
+  
+  def edit
+    @link = Link.find(params[:id])
+  end
+  
+  def update 
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      redirect_to links_path
+    else
+      flash[:error] = @link.errors.full_messages.join(' ')
+      redirect_to edit_link_path
+    end
+  end
+  
+  private
+  
+  def link_params
+    params.require(:link).permit(:title, :url)
   end
 end
